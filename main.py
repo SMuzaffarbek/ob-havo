ea36f3d8b2788c8fee657ced22b71b844f319d43
Muzaffarbek <88822010+SMuzaffarbek@users.noreply.github.com>
2021-12-17 12:48:37 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from obhavo import ObHavo
+
+#Parse from http://obhavo.uz
+
+# Ma'lumotlar
+
+# To'g'ri viloyat nomlari:
+'''
+tashkent
+jizzakh
+andijan
+namangan 
+ferghana
+gulistan
+bukhara 
+zarafshan
+karshi
+navoi
+nukus
+termez
+urgench
+khiva
+samarkand
+'''
+
+t = ObHavo("samarqand")
+
+print("Sana:", t.sana)
+print("Kunduzi", t.kunduz)
+print("Kechasi:", t.kechasi)
+print("Namlik:", t.namlik)
+print("Shamol:", t.shamol)
+print("Bosim:", t.bosim)
+print("Oy:", t.oy)
+print("Quyosh chiqishi:", t.chiqish)
+print("Quyosh botishi:", t.botish)
+print("Tong:", t.tong)
+print("Kun:", t.kun)
+print("Oqshom:", t.oqshom)
+
+# Haftalik ma'lumot
+
+print(t.haftalik())
